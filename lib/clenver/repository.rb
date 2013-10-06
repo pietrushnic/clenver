e9a3ec52b6bbbcaf47709117c39d27196a16eb0c
Piotr Król <pietrushnic@gmail.com>
2013-10-07 01:06:05 +0200

    add_remote support in Repository class

--- lib/clenver/repository.rb
+++ lib/clenver/repository.rb
@@ -5,15 +5,19 @@ class Repository
     @repo_uri = repo
     @dst = dst
     @abs_path = nil
+    @repo = nil
   end
 
   def clone
     repo_basename = File.basename("#{@repo_uri}",".git")
-    Git.clone(@repo_uri, repo_basename)
+    @repo = Git.clone(@repo_uri, repo_basename)
     @abs_path = Dir::pwd + "/" + repo_basename
   end
 
   def get_abs_path
     @abs_path
+  end
+  def add_remote(name, uri)
+    @repo.add_remote(name, uri)
   end
 end
