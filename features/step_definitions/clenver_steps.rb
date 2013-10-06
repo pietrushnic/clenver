c2dba1c1040e0d7cd29e3c8f27dca2b5a29265aa
Piotr Król <pietrushnic@gmail.com>
2013-10-07 01:04:57 +0200

    add_remote test scenarios steps

--- features/step_definitions/clenver_steps.rb
+++ features/step_definitions/clenver_steps.rb
@@ -13,4 +13,22 @@ Then(/^the following links should exist:$/) do |files|
   end
 end
 
+Then(/^the following remote uris should be connected in "(.*?)":$/) do |arg1, table|
+  table.raw.map{|repo| repo[0]}.each do |r|
+    curr_pwd = Dir::pwd
+    Dir::chdir("tmp/aruba" + "/" + arg1)
+    res = %x[git remote -v| grep #{r}|wc -l].strip
+    Dir::chdir(curr_pwd)
+    res.to_i.should eq(2)
+  end
+end
 
+Then(/^the following remote branches should be connected in "(.*?)":$/) do |arg1, table|
+  table.raw.map{|repo| repo[0]}.each do |r|
+    curr_pwd = Dir::pwd
+    Dir::chdir("tmp/aruba" + "/" + arg1)
+    res = %x[git remote -v| grep #{r}|wc -l].strip
+    Dir::chdir(curr_pwd)
+    res.to_i.should eq(2)
+  end
+end
