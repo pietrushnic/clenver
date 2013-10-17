3acfdc4a6097449c9da73ed3cb0a3e96e6b9e40f
Piotr Król <pietrushnic@gmail.com>
2013-10-18 00:42:55 +0200

    Add Runner abstraction level

---
+++ lib/clenver/runner.rb
@@ -0,0 +1,35 @@
+require 'gli'
+require 'clenver/project'
+require 'clenver/logging'
+
+module Clenver
+  class Runner
+    include Logging
+
+    def initialize(path, dst)
+      @path = path
+      @dst = dst
+    end
+
+    def start
+      if File.exist?(@path)
+        begin
+          yaml = Psych.load_file("#{@path}")
+          logger.debug("yaml: #{yaml}")
+          #TODO: create test and fix this place with check for empty file
+          p = Project.new(File.basename("#{@path}", ".yml"), yaml)
+          if @dst
+            p.create_repos(@dst)
+          else
+            p.create_repos()
+          end
+          p.init_repos
+        rescue Psych::SyntaxError => ex
+          exit_now!("#{@path}: syntax error : #{ex.message}", 1)
+        end
+      else
+        exit_now!("#{@path} no such file or directory", 2)
+      end
+    end
+  end
+end
