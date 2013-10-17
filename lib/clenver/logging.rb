63d163553c8fbed7c80397ec867524b00ef01e20
Piotr Król <pietrushnic@gmail.com>
2013-10-18 00:44:05 +0200

    Add logging

---
+++ lib/clenver/logging.rb
@@ -0,0 +1,24 @@
+require 'logger'
+
+module Logging
+  def logger
+    @logger ||= Logging.logger_for(self.class.name)
+  end
+
+  # Use a hash class-ivar to cache a unique Logger per class:
+  @loggers = {}
+
+  class << self
+    def logger_for(classname)
+      @loggers[classname] ||= configure_logger_for(classname)
+    end
+
+    def configure_logger_for(classname)
+      logger = Logger.new(STDOUT)
+      logger.progname = classname
+      logger.formatter = proc { |severity, datetime, progname, msg|
+        "[#{severity}] #{progname}: #{msg}\n"}
+      logger
+    end
+  end
+end
