b704133f54a86d557106b04ad981d33871cb877a
Piotr Król <pietrushnic@gmail.com>
2013-11-12 17:34:01 +0100

    improve comments in regression script

--- test/vbox/regression.sh
+++ test/vbox/regression.sh
@@ -14,8 +14,9 @@ sleep 45
 # cat host pub key to guest .ssh/authorized_keys
 pub_key=$(cat $HOME/.ssh/id_rsa.pub)
 pkey_scancodes=$(echo -en $pub_key|$SCANCODES)
+# echo "
 echo_sc="12 92 2e ae 23 a3 18 98 39 b9 2a 28 a8 aa 1c 9c"
-# >
+# " >
 gt_sc="2a 28 a8 aa 39 b9 2a 34 b4 aa 1c 9c"
 # mkdir -p $HOME/.ssh
 mkdir_ssh_pt="32 b2 25 a5 20 a0 17 97 13 93 39 b9 0c 8c 19 99 39 b9 2a 05 85 aa 2a 23 a3 aa 2a 18 98 aa 2a 32 b2 aa 2a 12 92 aa 35 b5 34 b4 1f 9f 1f 9f 23 a3 1c 9c"
