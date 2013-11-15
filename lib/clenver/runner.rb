34364d8119c49bf95a92e6cdcbfb59d5128f69a2
Piotr Król <pietrushnic@gmail.com>
2013-11-16 00:05:24 +0100

    fix pkgs string to handle multiple gems and packages

--- lib/clenver/runner.rb
+++ lib/clenver/runner.rb
@@ -20,17 +20,19 @@ module Clenver
           logger.debug("yaml: #{yaml}")
           #TODO: create test and fix this place with check for empty file
           p = Project.new(File.basename("#{@path}", ".yml"), yaml, @dst)
+          pkgs = ""
           unless yaml['apt'].nil?
             for pkg in yaml['apt'] do
-              pkgs = pkg + " "
+              pkgs = pkgs + " " + pkg + " "
             end
             puts pkgs
             p_mgr = PackageManger.new('apt', pkgs)
             p_mgr.install()
           end
+          pkgs = ""
           unless yaml['gem'].nil?
             for pkg in yaml['gem'] do
-              pkgs = pkg + " "
+              pkgs = pkgs + " " + pkg + " "
             end
             puts pkgs
             p_mgr = PackageManger.new('gem', pkgs)
