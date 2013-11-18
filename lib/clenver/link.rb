be2cf3962e5d69174c075a42ec80ff89a940387d
Piotr Król <pietrushnic@gmail.com>
2013-11-18 01:31:42 +0100

    add accessors and logging to link class

--- lib/clenver/link.rb
+++ lib/clenver/link.rb
@@ -1,21 +1,23 @@
 require 'fileutils'
+require 'clenver/logging'
 
 class Link
+  include Logging
   MAX_REPEAT = 3
+  attr_accessor :src, :dst
   def initialize(src,dst)
     @src = src
     @dst = expand_dst(dst)
   end
   def create
     puts "Link.create"
-    puts @dst
-    @dst.each do |d|
-      puts "src:#{@src}"
-      puts "d:#{d} "
+    dst.each do |d|
       i = 0
+      logger.debug("d:#{d}")
+      logger.debug("src:#{src}")
       while i < MAX_REPEAT do
         begin
-          File.symlink(@src, d.to_s)
+          File.symlink(src, d.to_s)
         rescue SystemCallError
           FileUtils.mv(d.to_s, d.to_s + "_old")
         else
@@ -31,6 +33,6 @@ class Link
       path = %x[ echo #{d}]
       ret.push(path.strip)
     end
-    return ret
+    ret
   end
 end
