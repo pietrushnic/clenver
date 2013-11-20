e535fe8233fe3b2a058eb32971f0dbdb2cf57c33
Piotr Król <pietrushnic@gmail.com>
2013-11-20 09:21:45 +0100

    fix env variable usage in Link class

--- lib/clenver/link.rb
+++ lib/clenver/link.rb
@@ -14,14 +14,16 @@ class Link
     puts "Link.create"
     dst.each do |d|
       i = 0
+      d = d.to_s.gsub(/\$\w+/) {|m| ENV[m[1..-1]]}
       logger.debug("d:#{d}")
-      logger.debug("src:#{src}")
+      logger.debug("src:#{src.gsub(/\$\w+/) {|n| ENV[n[1..-1]]}}")
       while i < MAX_REPEAT do
         begin
-          File.symlink(src, d.to_s)
+          File.symlink(src.gsub(/\$\w+/) {|n| ENV[n[1..-1]]}, d)
         rescue SystemCallError
-          FileUtils.mv(d.to_s, d.to_s + "_old")
+          FileUtils.mv(d, d + "_old")
         else
+          logge.debug("else")
           break
         end
         i += 1
